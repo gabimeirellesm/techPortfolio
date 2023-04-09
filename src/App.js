59e70c31783f54b5761096ff819a8f4b9bd41949
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-04-09 20:21:53 +0100

    created views and folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import Projects from "./Views/Projects";
+import Contact from "./Views/Contact";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <section id="section1">
+        <Home />
+      </section>
+      <section id="section2">
+        <About />
+      </section>
+      <section id="section3">
+        <Projects />
+      </section>
+      <section id="section4">
+        <Contact />
+      </section>
+      <Footer />
+      <style jsx="true">{`
+        section {
+          position: relative;
+          padding: 0;
+          margin: 0;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+        }
+        @media only screen and (max-width: 768px) {
+          #section {
+            width: 100%;
+            max-width: 100%;
+            margin: 0;
+            padding: 0;
+          }
+        }
+      `}</style>
     </div>
   );
 }
