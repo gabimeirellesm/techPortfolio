bee63809cad1b3c6dc7e4a839c7c993129b725d3
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-04-10 09:07:07 +0100

    updated style in section

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ function App() {
           position: relative;
           padding: 0;
           margin: 0;
-          top: 0;
-          left: 0;
           width: 100vw;
           height: 100vh;
         }
