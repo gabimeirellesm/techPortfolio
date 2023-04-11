55ba627e05e3520313e943b25a939b5b81e214c0
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2023-04-11 22:15:49 +0100

    updated font family

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Resume from "../Components/Resume";
 
 function Navbar() {
   const scrollToSection = (id) => {
@@ -7,78 +8,84 @@ function Navbar() {
   };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <button onClick={() => scrollToSection("section1")}>HOME</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection("section2")}>ABOUT</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection("section3")}>PROJECTS</button>
-        </li>
-        <li>
-          <button onClick={() => scrollToSection("section4")}>CONTACT</button>
-        </li>
-      </ul>
-      <style jsx>{`
-        nav {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          background-color: #fff;
-          box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-          z-index: 999;
-        }
+    <div>
+      <Resume />
 
-        ul {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          list-style: none;
-          margin: 0;
-          padding: 0;
-        }
+      <nav>
+        <ul>
+          <li>
+            <button onClick={() => scrollToSection("section1")}>HOME</button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection("section2")}>ABOUT</button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection("section3")}>
+              PROJECTS
+            </button>
+          </li>
+          <li>
+            <button onClick={() => scrollToSection("section4")}>CONTACT</button>
+          </li>
+        </ul>
 
-        li {
-          margin: 0 20px;
-        }
+        <style jsx="true">{`
+          @import url("https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap");
+          nav {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: 999;
+          }
 
-        button {
-          font-family: "NaifLostTrasure";
-          font-size: 24px;
-          font-weight: bold;
-          color: #555;
-          border: none;
-          background-color: transparent;
-          cursor: pointer;
-          padding: 0;
-          margin: 0;
-        }
+          ul {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
 
-        button:hover {
-          color: #f00;
-        }
+          li {
+            margin: 10px 20px;
+          }
 
-        button::after {
-          content: "";
-          display: block;
-          height: 4px;
-          width: 0%;
-          background-color: #f00;
-          transition: width 0.3s;
-        }
+          button {
+            font-family: "Share Tech Mono", monospace;
+            font-size: 1.5rem;
+            font-weight: bold;
+            color: #555;
+            border: none;
+            background-color: transparent;
+            cursor: pointer;
+            padding: 0;
+            margin: 0;
+          }
 
-        button:hover::after {
-          width: 100%;
-        }
-      `}</style>
-    </nav>
+          button:hover {
+            color: #f00;
+          }
+
+          button::after {
+            content: "";
+            display: block;
+            height: 4px;
+            width: 0%;
+            background-color: #f00;
+            transition: width 0.3s;
+          }
+
+          button:hover::after {
+            width: 100%;
+          }
+        `}</style>
+      </nav>
+    </div>
   );
 }
 
